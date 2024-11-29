a60b50233f1cb1a7a64bd0dfaa25da7dac28c3a3
austinserio <austin.c.serio@gmail.com>
2024-11-29 14:55:39 -0500

    Compatibility improvements and fixes

--- fadr.js
+++ fadr.js
@@ -1,66 +1,67 @@
 // JavaScript Document
 
-$("document").ready(function() {
-	var distFromTop = 0;
-	
-	var elements = [];
+$(document).ready(function() {
+    var distFromTop = 0;
 
-	//$( ".element-media" ).attr( "data-wow-iteration", 100000 );
+    // Initial check on page load
+    checkElements();
 
-	
-		var runOnce = false;
+    // Check again on scroll
+    $(window).scroll(function() {
+        checkElements();
+    });
 
-	//console.log(tags.hasClass(" wow "));
-	
-	$(window).scroll(function() {
-		//get current distance from top
-		distFromTop = $(window).scrollTop();
-		
-		for (var i = 0; i < $(".descriptor").length; i++) {
-			main($(".descriptor").eq(i));
-		}
-		
-		function main(el) {
+    // Function to check all .descriptor elements
+    function checkElements() {
+        // Get current distance from top
+        distFromTop = $(window).scrollTop();
 
-			if(distFromTop > Math.floor(el.offset().top * 0.85) && distFromTop < Math.floor(el.offset().top * 1.02)) {
-				moveIn(el);
+        $(".descriptor").each(function() {
+            main($(this));
+        });
+    }
 
-			}
-			if(distFromTop > Math.floor(el.offset().top * 1.02) || distFromTop < Math.floor(el.offset().top * 0.85)) {
-				moveOut(el);
-				console.log("move out")
-			}
-		}
-		
-		
-	});	
-	
-	function moveOut(el) {
-			var headers = el.find(".element-headers");
-			var media = el.find(".element-media");
+    function main(el) {
+        var elTop = el.offset().top;
+        var elHeight = el.outerHeight();
+        var windowHeight = $(window).height();
 
-			animate(headers);
-			animate(media);
-			function animate(element) {
-				element.removeClass("wow fadeInUp");
-				element.addClass("fadeOutUp animated");
-				element.css({"animation-name": "fadeOutUp", "-webkit-animation-name": "fadeOutUp", "animation-delay": "0s"});
+        // Adjusting the trigger zone based on the element's height
+        var triggerStart = elTop - windowHeight * 0.5; // Start animation when half of the element enters the viewport
+        var triggerEnd = elTop + elHeight - windowHeight * 0.42; // End animation when 10% of the element is still visible
 
-			}
-	}
-	
-	function moveIn(el) {
-			var headers = el.find(".element-headers");
-			var media = el.find(".element-media");
-			animate(headers);
-			animate(media);
-			function animate(element) {
-				element.removeClass("fadeOutUp");
-				
-					element.addClass("animated fadeInUp");
-					element.css({"animation-name": "fadeInUp", "-webkit-animation-name": "fadeInUp", "animation-delay": "0s"});
-				
-			}
-	}
-	
+        if (distFromTop > triggerStart && distFromTop < triggerEnd) {
+            moveIn(el);
+        } else {
+            moveOut(el);
+        }
+    }
+
+    function moveOut(el) {
+        var headers = el.find(".element-headers");
+        var media = el.find(".element-media");
+
+        animateOut(headers);
+        animateOut(media);
+    }
+
+    function moveIn(el) {
+        var headers = el.find(".element-headers");
+        var media = el.find(".element-media");
+
+        animateIn(headers);
+        animateIn(media);
+    }
+
+    function animateOut(element) {
+        element.removeClass("wow fadeInUp");
+        element.addClass("fadeOutUp animated");
+        element.css({"animation-name": "fadeOutUp", "-webkit-animation-name": "fadeOutUp", "animation-delay": "0s"});
+    }
+
+    function animateIn(element) {
+        element.removeClass("fadeOutUp");
+        element.addClass("animated fadeInUp");
+        element.css({"animation-name": "fadeInUp", "-webkit-animation-name": "fadeInUp", "animation-delay": "0s"});
+    }
 });
